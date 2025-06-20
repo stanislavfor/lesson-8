6e255e14c755116a28ac2be0d5635781281f6b58
stanislavfor <stasposter@narod.ru>
2025-06-21 02:37:17 +0300

    first commit

--- src/pages/CatalogPage.js
+++ src/pages/CatalogPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import ProductCard from './ProductCard';
+import ProductCard from '../components/ProductCard';
 
 const CatalogPage = () => {
     const [products, setProducts] = useState([
