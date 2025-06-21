f83574f7bba4bb70af6c0b83b8d4d6520962d9c7
stanislavfor <stasposter@narod.ru>
2025-06-21 14:39:08 +0300

    first commit

--- src/pages/CatalogPage.js
+++ src/pages/CatalogPage.js
@@ -9,6 +9,9 @@ const CatalogPage = () => {
         { id: 4, name: 'Product 4', price: 150, size: 'XS', image: '/images/product4.jpg' },
         { id: 5, name: 'Product 5', price: 250, size: 'S', image: '/images/product5.jpg' },
         { id: 6, name: 'Product 6', price: 350, size: 'M', image: '/images/product6.jpg' },
+        { id: 7, name: 'Product 7', price: 350, size: 'XL', image: '/images/product7.jpg' },
+        { id: 8, name: 'Product 8', price: 350, size: 'L', image: '/images/product8.jpg' },
+        { id: 9, name: 'Product 9', price: 350, size: 'XL', image: '/images/product9.jpg' }
     ]);
     const [filterSize, setFilterSize] = useState('');
 
@@ -27,6 +30,7 @@ const CatalogPage = () => {
                     <option value="S">S</option>
                     <option value="M">M</option>
                     <option value="L">L</option>
+                    <option value="XL">XL</option>
                 </select>
             </div>
             <div className="product-list">
